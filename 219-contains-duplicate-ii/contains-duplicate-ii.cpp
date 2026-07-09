89839d02ecb3f35318d48bb8c661ed09aca11bd6
dnvrmhra <dnvrmhra@gmail.com>
2026-07-09 15:53:50 +0530

    Time: 70 ms (77.51%) | Memory: 93.9 MB (67.44%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,4 +1,4 @@
-class Solution{
+/**class Solution{
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k){
         unordered_map<int,int>seen;
@@ -8,6 +8,24 @@ public:
                 return true;
             }
             seen[nums[i]]=i;
+        }
+        return false;
+    }
+};
+*/
+
+class Solution{
+    public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k){
+        unordered_set<int> window;
+        for (int i=0;i<nums.size();i++){
+            if (i>k){
+                window.erase(nums[i-k-1]);
+            }
+            if(window.count(nums[i])){
+                return true;
+            }
+            window.insert(nums[i]);
         }
         return false;
     }
