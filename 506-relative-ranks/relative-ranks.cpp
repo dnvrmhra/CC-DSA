3f2c410b644d8b3469405c07e7a5ce666da4e26f
Daanveer Mehra <dnvrmhra@gmail.com>
2026-09-24 09:53:12 +0530

    Time: 23 ms (10.54%) | Memory: 16 MB (47.19%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score){
+        vector<string> answer;
+        vector<int> sorted=score;
+        sort(sorted.begin(), sorted.end(), greater<>());
+        for (int i=0;i<score.size();i++){
+            int rank=find(sorted.begin(), sorted.end(), score[i])-sorted.begin()+1;
+            if (rank==1){
+                answer.push_back("Gold Medal");
+            }
+            else if (rank==2){
+                answer.push_back("Silver Medal");
+            }
+            else if (rank==3){
+                answer.push_back("Bronze Medal");
+            }
+            else {
+                answer.push_back(to_string(rank));
+            }
+        }
+        return answer;
+    }
+};
